2285e455d8f378ff22cbec83b1cf7f4d1c73197d
Vicko Franić <vicko@q-alliance.com>
2018-03-10 18:39:35 +0100

    Additional members and menu updates

--- src/app/pages/band/band-members.ts
+++ src/app/pages/band/band-members.ts
@@ -12,5 +12,47 @@ export const bandMembers: any = [
     avatar: 'assets/images/dinko.png',
     description: 'Dinko se glazbom bavi još od osnovne škole,rockabilly meštar o\' gitare je. Ima gitaru na Brian Setzera.',
     instruments: 'Električna i akustična gitara / Vokal'
-  }
+  },
+  {
+    first_name: 'Ivan',
+    last_name: 'Gilić',
+    avatar: 'assets/images/gile.png',
+    description: 'Opis',
+    instruments: 'Klavijature / Vokal'
+  },
+  {
+    first_name: 'Željko',
+    last_name: 'Mitrović',
+    avatar: 'assets/images/mitar.png',
+    description: 'Opis',
+    instruments: 'Bas gitara'
+  },
+  {
+    first_name: 'Mijo',
+    last_name: 'Vrvilo',
+    avatar: 'assets/images/mijo.png',
+    description: 'Opis',
+    instruments: 'Bubnjevi / udaraljke / audio samples'
+  },
+  {
+    first_name: 'Kristina',
+    last_name: 'Ujević Nikolac',
+    avatar: 'assets/images/kike.png',
+    description: 'Opis',
+    instruments: 'Vokal'
+  },
+  {
+    first_name: 'Bojan',
+    last_name: 'Delač',
+    avatar: 'assets/images/bojan.png',
+    description: 'Opis',
+    instruments: 'Saksofon'
+  },
+  {
+    first_name: 'Edi',
+    last_name: 'Ferjan',
+    avatar: 'assets/images/edi.png',
+    description: 'Opis',
+    instruments: 'VJ / Video projekcije'
+  },
 ];
